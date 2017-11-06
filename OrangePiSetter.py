537d370129414df7b7582dfc585e0189f6beca8c
Petr Kalinin <petr@kalinin.nnov.ru>
2017-11-06 20:27:48 +0300

    Added OrangePiSetter

---
+++ OrangePiSetter.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+
+leds = (port.PA12, port.PA11, port.PA6)
+
+gpio.init()
+for led in leds:
+    gpio.setcfg(led, gpio.OUTPUT)
+
+
+if not os.getegid() == 0:
+    sys.exit('Script must be run as root')
+
+
+class DummySetter:
+    def __call__(self, *values):
+        for i in range(len(leds)):
+            gpio.output(leds[i], values[i])
